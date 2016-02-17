a0bf2e64a6beaca1f9374486cf433e5b28e8b799
dmboy <893658963@qq.com>
2016-02-17 13:27:09 +0800

    add some icons

--- app/src/main/java/com/example/asus/mdcommunity/MainActivity.java
+++ app/src/main/java/com/example/asus/mdcommunity/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -114,5 +115,27 @@ public class MainActivity extends AppCompatActivity {
         myFragments.add(businessFragment);
         myFragments.add(activityFragment);
         myFragments.add(noticeFragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

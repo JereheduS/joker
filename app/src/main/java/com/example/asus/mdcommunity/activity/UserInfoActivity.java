e9728b5ed91e6bd67b03d66956c546fa7cd8e579
dmboy <893658963@qq.com>
2016-02-17 17:16:08 +0800

    tab color

--- app/src/main/java/com/example/asus/mdcommunity/activity/UserInfoActivity.java
+++ app/src/main/java/com/example/asus/mdcommunity/activity/UserInfoActivity.java
@@ -1,27 +1,55 @@
 package com.example.asus.mdcommunity.activity;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.transition.Explode;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.example.asus.mdcommunity.R;
+import com.example.asus.mdcommunity.adapter.MyFragmentAdapter;
 
-public class UserInfoActivity extends Activity {
+public class UserInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
         // 设置转场动画
         getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        super.onCreate(savedInstanceState);
+
         getWindow().setEnterTransition(new Explode());
         getWindow().setExitTransition(new Explode());
 
         setContentView(R.layout.activity_user_info);
+
+        // 初始化视图
+        initView();
+    }
+
+    private void initView(){
+        // init toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("个人信息");
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_36dp);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserInfoActivity.this.finishAfterTransition();
+            }
+        });
     }
 
     @Override
